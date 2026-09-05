db25fc1299ce71ac32d0972284ff34e82cc1520c
nesruzenu <nesru16zeynu@gmail.com>
2026-09-05 22:07:42 +0300

    commit

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
-"""Unittests for models/base_model.py."""
+"""
+Test suits for the base model
+"""
+
+import os
+import re
+import json
+import uuid
 import unittest
-from models.base_model import BaseModel
+from time import sleep
 from datetime import datetime
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test suite for BaseModel initialization and methods."""
+    """
+    Tests attributes of the base model
+    """
 
-    def test_init_no_args(self):
-        bm = BaseModel()
-        self.assertIsInstance(bm.id, str)
-        self.assertIsInstance(bm.created_at, datetime)
-        self.assertIsInstance(bm.updated_at, datetime)
+    def setUp(self):
+        """
+        Classes needed for testing
+        """
+        pass
 
-    def test_to_dict(self):
-        bm = BaseModel()
-        bm_dict = bm.to_dict()
-        self.assertEqual(bm_dict["__class__"], "BaseModel")
-        self.assertIsInstance(bm_dict["created_at"], str)
+    def test_basic(self):
+        """
+        Tests basic imputs for the BaseModel class
+        """
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number],
+                         ["ALX", 89])
 
-    def test_str_representation(self):
-        bm = BaseModel()
-        self.assertIn("[BaseModel]", str(bm))
-        self.assertIn(bm.id, str(bm))
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
+    
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
