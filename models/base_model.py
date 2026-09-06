3cd4c555ff3a8db8a51598631e5f9d1a2ee777f7
nesruzenu <nesru16zeynu@gmail.com>
2026-09-07 00:51:01 +0300

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""
-This module defines the BaseModel class for the AirBnB project.
-"""
+"""Defines the BaseModel class."""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
+    """Base class that defines all common attributes/methods
+    for other classes.
+    """
 
-    def __init__(self, *args, **kwargs):
-        """Initializes a new BaseModel instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+    def __init__(self):
+        """Initialize a new instance of BaseModel."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns the string representation of the instance."""
+        """Return the string representation of the BaseModel instance."""
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with the current datetime and saves storage."""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        models.storage.new(self)
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of __dict__
+        of the instance, with created_at and updated_at converted to
+        ISO format strings, and __class__ added.
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = type(self).__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
