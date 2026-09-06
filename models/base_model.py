a3a63e433f851c111fa64eeb5ba3d993eae51c49
nesruzenu <nesru16zeynu@gmail.com>
2026-09-07 01:00:20 +0300

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines the BaseModel class."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -9,11 +10,28 @@ class BaseModel:
     for other classes.
     """
 
-    def __init__(self):
-        """Initialize a new instance of BaseModel."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel.
+
+        Args:
+            *args: Unused.
+            **kwargs: Key/value pairs of attributes to set. If not empty,
+                each key (except __class__) is set as an instance
+                attribute, with created_at and updated_at converted
+                from ISO format strings to datetime objects.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return the string representation of the BaseModel instance."""
@@ -21,8 +39,11 @@ class BaseModel:
             type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with the current datetime and save to
+        storage.
+        """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of __dict__
@@ -33,13 +54,4 @@ class BaseModel:
         new_dict["__class__"] = type(self).__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        return new_dict        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__."""
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        return dict_copy
+        return new_dict
