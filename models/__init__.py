446481bd20a5af783b4a8e1bf71a1fded1b35198
nesruzenu <nesru16zeynu@gmail.com>
2026-09-07 01:02:53 +0300

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Initialize the models package and set up FileStorage.
-"""
+"""Creates a unique FileStorage instance for the application."""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
